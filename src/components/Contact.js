9bf0c386d8980d12f821e3a9175ff99495734eb1
ivan-comar <guardian.sanddy@gmail.com>
2017-11-24 00:51:24 +0900

    add contact.js file

---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Contact () {
+  return (
+    <div>
+      <h3>Phone: 888.888.888</h3>
+      <h3>Address: Houston, TX</h3>
+      <h3>Email: ammar@ammarselophotography.com</h3>
+      <style jsx>{`
+     	div {
+	       margin: 10%;
+	    }
+      `}</style>
+    </div>
+  )
+}
+
+export default Contact
